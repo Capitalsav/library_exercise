a9912782a64f0acbdecb333283adaacde3cd28d9
Anton <capitalsav@gmail.com>
2018-05-13 22:00:53 +0300

    Update comments controller tests with JSON check

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe CommentsController, type: :controller do
+  let(:json) { JSON.parse(response.body) }
   before(:each) do
     @user = create(:user)
     sign_in @user
@@ -14,6 +15,17 @@ RSpec.describe CommentsController, type: :controller do
             comment: attributes_for(:comment, book_id: @book.id, text: 'Text'),
             book_id: @book.id
         }
+      end.to change { Comment.count }.by(1) and
+          have_http_status(200)
+    end
+
+    it 'creates comment with JSON' do
+      expect do
+        post :create, params: {
+            comment: attributes_for(:comment, book_id: @book.id, text: 'Text'),
+            book_id: @book.id
+        },
+        format: :json
       end.to change { Comment.count }.by(1) and
           have_http_status(200)
     end
