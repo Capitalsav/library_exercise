24b890a3ae9695dcc27691155fcdef16df613726
Anton <capitalsav@gmail.com>
2018-05-25 17:43:04 +0300

    Make user unable to upload avatar (for Heroku)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
     devise_parameter_sanitizer.permit(:account_update,
-                                      keys: %i[name email password password_confirmation current_password avatar])
+                                      keys: %i[name email password password_confirmation current_password])
   end
 end
